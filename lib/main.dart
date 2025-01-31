6441322d10740106abafddf623268314fb5dfeb2
nabilwics <nabilwicaksono274@gmail.com>
2025-01-31 10:36:33 +0700

    update main dart and add .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:my_flutter_app/ui/home/pages/home_page.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env");
+
+  // Set the Bloc observer globally
+  Bloc.observer = AppBlocObserver();
+
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Info Games',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
       ),
+      home: Homepage(),
     );
+  }
+}
+
+// Define AppBlocObserver if not already defined
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    print('Bloc Created: ${bloc.runtimeType}');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('Bloc State Changed: ${bloc.runtimeType}, $change');
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    print('Bloc Closed: ${bloc.runtimeType}');
   }
 }
